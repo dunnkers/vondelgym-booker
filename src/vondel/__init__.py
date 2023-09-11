f4a0625c58a61ed57b0cc38440f89c79d800a4a5
Jeroen Overschie <jeroenoverschie@gmail.com>
2023-09-11 11:48:01 +0200

    Go non-async

--- src/vondel/__init__.py
+++ src/vondel/__init__.py
@@ -3,7 +3,6 @@ __version__="0.0.1"
 from vondel.user import User
 from vondel.parse_page import get_wanted_classes_from_vondelgym_oost
 import logging
-import asyncio
 
 def run(email: str, password: str) -> None:
   logger = logging.getLogger()
@@ -15,9 +14,7 @@ def run(email: str, password: str) -> None:
   
   logger.info("Found following classes:")
 
-  tasks = []
   for clazz in wanted:
     logger.info(f"Trying to book: {clazz}")
-    tasks.append(user.book_class_at(clazz.start_time, clazz.day, clazz.month))
+    user.book_class_at(clazz.start_time, clazz.day, clazz.month)
 
-  asyncio.gather(*tasks)
